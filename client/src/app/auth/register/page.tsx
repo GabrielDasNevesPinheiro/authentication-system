057e8636e995b1526b73ca9c3d378b92af551899
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-26 22:23:23 -0300

    register page base layout

--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -1,7 +1,35 @@
-export default function RegisterPage() {
+"use client";
+
+import Button from "@/components/button";
+import React, { useState } from "react";
+
+export default function LoginPage() {
+
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [secondPassword, setSecondPassword] = useState<string>('');
+
+    function usernameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setUsername(event.target.value);
+    }
+    function passwordChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value);
+    }
+    function secondPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setSecondPassword(event.target.value);
+    }
+    function submit() {
+        console.log(`${username} ${password} ${secondPassword}`);
+    }
+
     return (
-        <div className="flex flex-col">
-            <h1>Register Page</h1>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="flex flex-col items-center space-y-4">
+                <input onChange={usernameChange} type="text" name="username" className="border rounded-2xl text-center px-4 py-2" placeholder="Username here"/>
+                <input onChange={passwordChange} type="password" name="password" className="border rounded-2xl text-center px-4 py-2" placeholder="passw***"/>
+                <input onChange={secondPasswordChange} type="password" name="passwordConfirm" className="border rounded-2xl text-center px-4 py-2" placeholder="Confirm password"/>
+                <Button onClick={submit}>Create Account</Button>
+            </div>
         </div>
     );
 }
