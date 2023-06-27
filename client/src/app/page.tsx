314db5a47356924a541536da0ef8f10b462f0363
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-26 21:43:07 -0300

    index page tailwind styled

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+
 export default function Home() {
+
+    const router = useRouter();
+
   return (
     <div className="flex flex-col h-screen w-screen justify-center">
       <div className="flex flex-col p-6 items-center space-y-8">
-        <h1 className="text-4xl">Bem vindo(a)</h1>
+        <h1 className="text-4xl">Welcome!</h1>
         <div className="flex  space-x-2 text-xl">
-          <button className="rounded-xl py-2 w-28 bg-red-600">Login</button>
-          <button className="rounded-xl py-2 w-28 bg-red-600">Register</button>
+          <button className="rounded-xl py-2 w-28 bg-red-600" onClick={() => { router.push("/auth/login") }}>Login</button>
+          <button className="rounded-xl py-2 w-28 bg-red-600" onClick={() => { router.push("/auth/register") }}>Register</button>
         </div>
       </div>
     </div>
