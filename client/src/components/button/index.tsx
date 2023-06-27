9cc5f055275d9e8d45fdb0b8d475d3b37c900546
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-26 22:25:48 -0300

    button padding fix

--- client/src/components/button/index.tsx
+++ client/src/components/button/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Button({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
     return (
-        <button className="rounded-xl py-2 w-28 bg-red-600" onClick={onClick}>
+        <button className="rounded-xl py-2 px-4 w-32 bg-red-600" onClick={onClick}>
             {children}
         </button>
     )
