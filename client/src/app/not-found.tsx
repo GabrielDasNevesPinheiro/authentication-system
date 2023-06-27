1fc3abc1858284ad7706fc8239440344be3361f2
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-27 17:28:57 -0300

    404 page timeout

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function Notfound() {
+
+    const router = useRouter();
+
+    setTimeout(() => {
+        router.push("/");
+    }, 5000);
 
     return (
         <div className="flex flex-col items-center h-screen justify-center text-4xl">
