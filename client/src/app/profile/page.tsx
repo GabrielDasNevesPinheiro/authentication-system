5a5b1ac421a0ba92a0f4f278aa7f8685d5f4156a
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-26 22:30:31 -0300

    profile page base layout

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import Button from "@/components/button";
+
 export default function ProfilePage() {
+    const username = "idk yet";
+
+    function logout() {
+        console.log('here you will logout');
+    }
+
     return (
-        <div className="flex flex-col">
-            <h1>Profile Page</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="flex flex-col space-y-4 items-center">
+                <h1 className="text-4xl">Logged as: {username}</h1>
+                <Button onClick={logout}>Logout</Button>
+            </div>
         </div>
     );
 }
