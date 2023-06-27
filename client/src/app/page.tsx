9f621f38ac21f4322bbf79983e1dcbc99627adb7
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-26 21:32:00 -0300

    index page design

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen">
-      <div>
-        <h1>Bem vindo(a)</h1>
+    <div className="flex flex-col h-screen w-screen justify-center">
+      <div className="flex flex-col p-6 items-center space-y-8">
+        <h1 className="text-4xl">Bem vindo(a)</h1>
+        <div className="flex  space-x-2 text-xl">
+          <button className="rounded-xl py-2 w-28 bg-red-600">Login</button>
+          <button className="rounded-xl py-2 w-28 bg-red-600">Register</button>
+        </div>
       </div>
     </div>
   );
