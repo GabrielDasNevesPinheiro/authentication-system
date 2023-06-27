d1da5d834c500d4067a2b031a53f52c7796ce57f
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-26 21:10:19 -0300

    NextJS initial Project

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default function Notfound() {
+
+    return (
+        <h1>Not Found</h1>
+    );
+
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <div>
+        <h1>Bem vindo(a)</h1>
+      </div>
+    </div>
+  );
+}
