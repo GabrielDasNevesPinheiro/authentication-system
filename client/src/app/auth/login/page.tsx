6bccce67bc052855b828eb73419d618396641d81
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-28 14:27:14 -0300

    user experience

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -24,12 +24,13 @@ export default function LoginPage() {
     
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [output, setOutput] = useState<string>('');
 
     function usernameChange(event: React.ChangeEvent<HTMLInputElement>) {
         setUsername(event.target.value);
     }
     function passwordChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setPassword(event.target.value)
+        setPassword(event.target.value);
     }
     async function submit() {
         
@@ -49,18 +50,22 @@ export default function LoginPage() {
             mode: 'cors'
         })).json();
         
-        if(res.message) dispatch(login(res.token));
+        if(!res.message) return setOutput('Invalid data');
+
+        dispatch(login(res.token));
         router.refresh();
 
+
     }
-    
 
     if (token) router.push("/profile");
+    
     
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <div className="flex flex-col items-center space-y-4">
+                <h1 className="text-4xl">{output}</h1>
                 <input onChange={usernameChange} type="text" name="username" id=""  className="border rounded-2xl text-center px-4 py-2" placeholder="Username here"/>
                 <input onChange={passwordChange} type="password" name="password" id="" className="border rounded-2xl text-center px-4 py-2" placeholder="passw***"/>
                 <Button onClick={submit}>Login</Button>
