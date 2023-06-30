13845894856da77d198f2d88589021450e9319ff
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-06-30 16:11:20 -0300

    Old Client project

--- client/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        SERVER_URL: process.env.SERVER_URL,
-        API_KEY: process.env.API_KEY,
-    }
-}
-
-module.exports = nextConfig
--- client/src/components/button/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Button({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
-    return (
-        <button className="rounded-xl py-2 px-4 w-32 bg-red-600" onClick={onClick}>
-            {children}
-        </button>
-    )
-}
